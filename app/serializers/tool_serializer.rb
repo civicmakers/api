d2bdbd7d7f6da05fe064ba77c4c38ccf87ff2c86
Ben <benhartney85@gmail.com>
2015-07-30 20:25:39 -0700

    Update tool serializer

--- app/serializers/tool_serializer.rb
+++ app/serializers/tool_serializer.rb
@@ -1,3 +1,13 @@
-class PostSerializer < ActiveModel::Serializer
-  attributes :name
+class ToolSerializer < ActiveModel::Serializer
+  attributes :name, :description, :url, :social_links
+  attribute :guid, key: :id
+
+  def social_links
+    {
+      facebook: object.facebook_url,
+      twitter: object.twitter_url,
+      linkedin: object.twitter_url
+    }
+  end
+
 end
