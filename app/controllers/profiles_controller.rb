0c1839a690ef3d341322ead4872a515cf517ceed
Ben <benhartney85@gmail.com>
2015-07-31 17:50:58 -0700

    Add /profiles/:id

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,4 +5,9 @@ class ProfilesController < ApplicationController
     render json: @profiles, include: params[:include]
   end
 
+  def show
+    @profile = Profile.find(params[:id])
+    render json: @profile, include: params[:include]
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :tools, only: [:index, :show]
 
-  resources :profiles, only: [:index]
+  resources :profiles, only: [:index, :show]
 
   resources :projects, only: [:index]
 
